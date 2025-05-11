fa539421f91db25bf5b766050e3ed1c0e72b851a
username <jenhussein@ucdavis.edu>
2025-05-10 20:43:08 -0700

    update

--- rosalind/strings.py
+++ rosalind/strings.py
@@ -1,2 +1,9 @@
-ls
-s
+import math
+
+s = 'FdsfHomalopsisYBf8E2Es9BG88bcheVTdCL7cKbUf3F2tnwMxTj667lJlNrVfj7NrWoLsyPWMhyr7PUsBSLjHhsfzV1jkHkcHugQGRn01LcWYS5QgUdt80HEffL67mPNYNhEPZU2UxC0UUOrr3t8sBuTbv9TasperumjI.'
+def string_idx(s,a,b,c,d):
+	half1 = s[a:b+1]
+	half2 = s[c:d+1]
+	return half1 + " " + half2
+	
+print(string_idx(s, 4, 13, 157, 163))
