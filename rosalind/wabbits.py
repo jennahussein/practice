418959916710d93928b3eaa30a13a5c52397c997
Sana Hussein <sana@Sanas-MacBook-Pro-2.local>
2025-06-03 10:07:58 -0700

    update

--- rosalind/wabbits.py
+++ rosalind/wabbits.py
@@ -1,8 +1,15 @@
 import math
-   
+
+"""
+given positive integers n <= 40 and k <=5, return the total number of rabbit
+pairs that will be present after n months if we begin with 1 pair and in each
+generation, every pair of reproduction age rabbits produces a littler of k rabbit 
+pairs instead of only one pair
+"""
+
 def mod_fib(n, k):
-	fib = [0] * (n + 1)
-	fib[1] = 1
+	fib = [0] * (n + 1) #made a list to hold count of rabbits
+	fib[1] = 1 #says we begin with 1 pair
 	if n >= 2:
 		fib[2] = 1
 	for i in range (k, n + 1):
